601e2f9da585b0a4ca38f8fa2864f112b61677a5
EekshithPavan <eekshith.pavan@izenix.com>
2023-12-13 17:25:11 +0530

    Captch code added

---
+++ assets/js/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = izenix.com;
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: false }));
+
+app.post('/upload', function (req, res) {
+    const params = new URLSearchParams({
+        secret: '6LfuuC8pAAAAAGEzwHH4cwupUD1KYARjq5Dd3Upp',
+        response: req.body['g-recaptcha-response'],
+        remoteip: req.ip
+    }); 
+
+    fetch('https://www.google.com/recaptcha/api/siteverify', {
+        method: "POST",
+        body: params,
+    })
+        .then(res => res.json())
+        .then(data => {
+            if (data.success) {
+                res.json({ captchaSuccess: true });
+            } else {
+                res.json({ captchaSuccess: false });
+
+            }
+        })
+});
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}`)
+})
